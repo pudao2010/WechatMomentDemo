384a40583dbe8dd6b1e21c55c6c6f5b729c19d7c
蒲成 <pucheng@doupai.cc>
2017-09-05 19:24:50 +0800

    增加util工具类包

---
+++ app/src/main/java/com/example/pudao/wechatmomentdemo/util/ViewUtil.java
@@ -0,0 +1,8 @@
+package com.example.pudao.wechatmomentdemo.util;
+
+/**
+ * Created by pucheng on 2017/9/5.
+ */
+
+public class ViewUtil {
+}
--- app/src/main/java/com/example/pudao/wechatmomentdemo/widget/ImagesLayout.java
+++ app/src/main/java/com/example/pudao/wechatmomentdemo/widget/ImagesLayout.java
@@ -1,9 +1,28 @@
 package com.example.pudao.wechatmomentdemo.widget;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
 /**
  * Created by pucheng on 2017/9/5.
  * 自定义图片布局
  */
 
-public class ImagesLayout {
+public class ImagesLayout extends ViewGroup{
+
+
+
+    public ImagesLayout(Context context) {
+        this(context, null);
+    }
+
+    public ImagesLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
 }
